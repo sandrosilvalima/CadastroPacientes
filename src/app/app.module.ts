854436f7c94b9dc0d636415f130c07e29248cdc5
Sandro Silva Lima <88206798+sandrosilvalima@users.noreply.github.com>
2021-11-26 23:59:07 -0300

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CadastroPacienteComponent } from './cadastro-paciente/cadastro-paciente.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CadastroPacienteComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/cadastro-paciente/cadastro-paciente.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastro-paciente',
+  templateUrl: './cadastro-paciente.component.html',
+  styleUrls: ['./cadastro-paciente.component.css']
+})
+export class CadastroPacienteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
